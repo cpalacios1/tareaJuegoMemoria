9ad1921c069e9ac11a14abcaa7bdc35cc65ed3c6
Christian <christian@toracom.net>
2016-02-03 14:58:04 -0600

    Initial Commit

---
+++ TareaJuegoMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TareaJuegoMemoria
+//
+//  Created by toracom Desarrollador Cuatro on 2/3/16.
+//  Copyright © 2016 toracom Desarrollador Cuatro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
