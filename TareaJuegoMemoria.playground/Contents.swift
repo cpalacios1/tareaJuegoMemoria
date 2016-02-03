97c43a619c378cd05161b20b0040a51214364ee8
Christian <christian@toracom.net>
2016-02-03 16:14:19 -0600

    Se modificaron archivos

---
+++ TareaJuegoMemoria.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for i in 0...100 {
+    if i%5 == 0 {
+        print("\(i)\tBingo!!!")
+    }
+    if i%2 == 0 {
+        print("\(i)\tpar!!!")
+    }
+    if i%2 != 0 {
+        print("\(i)\timpar!!!")
+    }
+    if i >= 30 && i <= 40 {
+        print("\(i)\tViva Swift!!!")
+    }
+}
